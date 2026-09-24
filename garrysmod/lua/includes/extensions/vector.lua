9611f91f3c8c48c996d152ceb9bb56543a45440d
Deyan Dobromirov <dvd_video@abv.bg>
2026-09-24 16:01:13 +0300

    Updated: Vector to color conversion

--- garrysmod/lua/includes/extensions/vector.lua
+++ garrysmod/lua/includes/extensions/vector.lua
@@ -2,11 +2,11 @@
 local meta = FindMetaTable( "Vector" )
 
 --[[---------------------------------------------------------
-	Converts Vector To Color - alpha precision lost, must reset
+	Converts a vector object to a color with alpha as argument
 -----------------------------------------------------------]]
-function meta:ToColor()
+function meta:ToColor( num )
 
-	local x, y, z = meta.Unpack( self )
-	return Color( x * 255, y * 255, z * 255 )
+	local x, y, z = self:Unpack()
+	return Color( x * 255, y * 255, z * 255, ( num == nil && 1 || num ) * 255 )
 
 end
